dc30f6caa9019e7787d2f598de7dadb42e2cada9
ChangJoo Park <pcjpcj2@gmail.com>
2020-01-08 21:33:58 +0900

    organize imports

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,24 +1,19 @@
 library home_view;
 
 import 'dart:async';
-import 'dart:io';
 
 import 'package:auto_animated/auto_animated.dart';
 import 'package:date_format/date_format.dart';
 import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:keyboard_visibility/keyboard_visibility.dart';
 import 'package:logger/logger.dart';
 import 'package:next_page/core/logger.dart';
 import 'package:next_page/models/note.dart';
 import 'package:next_page/views/note_detail/note_detail_view.dart';
 import 'package:next_page/views/setting/setting_view.dart';
-import 'package:next_page/widgets/bottom_action_bar.dart';
 import 'package:provider_architecture/provider_architecture.dart';
 import 'package:responsive_builder/responsive_builder.dart';
-import 'package:unicorndial/unicorndial.dart';
 
 import 'home_view_model.dart';
 

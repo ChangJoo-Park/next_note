3c54098ad42f4cf4901577336a6c698453e665fe
ChangJoo Park <pcjpcj2@gmail.com>
2020-01-08 21:35:13 +0900

    extract note list tile

--- lib/views/home/home_mobile.dart
+++ lib/views/home/home_mobile.dart
@@ -286,28 +286,3 @@ class __HomeMobileState extends State<_HomeMobile> {
     return nowString;
   }
 }
-
-class NoteListTile extends StatelessWidget {
-  const NoteListTile({
-    Key key,
-    @required this.note,
-    this.onTap,
-    this.onLongPress,
-  }) : super(key: key);
-
-  final Note note;
-  final VoidCallback onTap;
-  final VoidCallback onLongPress;
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        note.fileName,
-        style: TextStyle(fontWeight: FontWeight.bold),
-      ),
-      subtitle: Text(note.modified.toString()),
-      onTap: onTap,
-      onLongPress: onLongPress,
-    );
-  }
-}
